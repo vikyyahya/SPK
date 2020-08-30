e1fc5ffcb3f25606f10cdc28ddc9ac6a63654721
vikyyahya <vikyyahya.id@gmail.com>
2020-08-30 13:17:11 +0700

    ss

--- app/Http/Controllers/PerangkinganController.php
+++ app/Http/Controllers/PerangkinganController.php
@@ -8,6 +8,8 @@ use App\Bobot;
 use App\Penawaran;
 use App\User;
 use App\Tender;
+use \stdClass;
+
 
 
 class PerangkinganController extends Controller
@@ -39,20 +41,54 @@ class PerangkinganController extends Controller
         //sum value
         $jmlbobot = Bobot::where('id_tender',$id)->sum('nilai');
         //cari rata2 kriteria
-        $rkriteria = [];
+        $rkriteria = new stdClass();
         foreach ($bobotrata as $bobot){ 
-            $rata2 = $jmlbobot/$bobot->nilai;
-            array_push($rkriteria,[$bobot->deskripsi => $rata2 ]);
+            $rata2 = $bobot->nilai /$jmlbobot;
+            // array_push($rkriteria,[$bobot->deskripsi => $rata2 ]);
+            $variable = $bobot->deskripsi;
+            $rkriteria->$variable = $rata2;
             }
 
         //pangkat value
-        
+        $vektors = [];
+
         foreach ($penawaran as $pen){ 
-
+            $jangPembayaran = $pen->pembayaran;
+            $kualitas = $pen->kualitas;
+            $vpembayaran = '';
+            $vkualitas = '';
+            if($jangPembayaran == 'cash'){
+                $vpembayaran = 1;
+            }else if($jangPembayaran == 'tempo 14 hari'){
+                $vpembayaran = 2;
+            }else if($jangPembayaran == 'tempo 30 hari'){
+                $vpembayaran = 3;
+            }else if($jangPembayaran == 'tempo 60 hari'){
+                $vpembayaran = 4;
+            } 
+            if($kualitas == 'bagus'){
+                $vkualitas = 3;
+            }else if($kualitas == 'sedang'){
+                $vkualitas = 2;
+            }else if($kualitas == 'rendah'){
+                $vkualitas = 1;
             }
+            
+            $hasilpembayaran = "";
+            $totalVektor= "";
+            array_push($vektors,['id_iser'=> $pen->id_user,
+            'pembayaran' => pow($vpembayaran,$rkriteria->{'Jangka Waktu Pembayaran'} ) ,
+            'harga' => pow($pen->harga, $rkriteria->{'Harga'}),
+            'kualitas' => pow($vkualitas,$rkriteria->{'Kualitas'}) ,
+            'status_stock' => $vpembayaran/$rkriteria->{'Status Stok Barang'},
+            ]);
+            
+        }
+           
+        // return print_r($rkriteria);
+        return $vektors;
 
 
-        return $penawaran;
 
         return view('perangkingna.perangkingna', ['bobot' => $bobot]);
     }
